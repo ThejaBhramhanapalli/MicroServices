9d5e134dff1e4635636348918a188686c98447cb
ThejaBrahmanapalli <btheja123@gmail.com>
2020-04-23 16:06:42 +0530

    Adding new Comments

--- user-api/src/main/java/com/bank/user/UserApiApplication.java
+++ user-api/src/main/java/com/bank/user/UserApiApplication.java
@@ -11,6 +11,7 @@ public class UserApiApplication implements CommandLineRunner{
 		SpringApplication.run(UserApiApplication.class, args);
 	}
 
+	
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.print("Using Run Method Command line runner" );
